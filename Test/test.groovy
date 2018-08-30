9caa4eb2b878041b10cbcb674a0ae6cbf923221a
photoretouch <photoretouch@photoretouch-no-Mac-Pro.local>
2018-08-30 17:18:09 +0900

    Success 住所結合成功

--- Test/test.groovy
+++ Test/test.groovy
@@ -3,12 +3,30 @@ env.injectionOne = {cassette, record, labelList, imageTable ->
  myInjectionOne(cassette, record, labelList, imageTable);
 };
 if(!env.multiLayout) {
- doProduce(1, env.imageTable); // ページ数 1
+ doProduce(2, env.imageTable); // ページ数 1
 }
 
 //独自の刺し込み処理
 def myInjectionOne(cassette, record, labelList, imageTable) {
+  //ラベルの追加
+  def additionalLabelList = ["結合住所"];
+
+  //表面住所データ結合
+  def postnum = record['郵便番号'];
+  def adr1 = record['住所1'];
+  def adr2 = record['住所2'];
+  def address = postnum + adr1 + adr2;
+
+  record['結合住所'] = address;
+
+  //基本関数
  labelList.each {
- injectionOneParts(cassette, it , record, imageTable);
+   injectionOneParts(cassette, it , record, imageTable);
  }
+
+ //追加ラベルへの差し込み
+ additionalLabelList.each{
+   injectionOneParts(cassette, it , record, imageTable);
+ }
+
 }

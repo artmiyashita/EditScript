033bb5d679a54314e5e61c75630dda257a0679fe
photoretouch <photoretouch@photoretouch-no-Mac-Pro.local>
2018-08-30 15:58:49 +0900

    commit test

--- Test/test.groovy
+++ Test/test.groovy
@@ -5,6 +5,7 @@ env.injectionOne = {cassette, record, labelList, imageTable ->
 if(!env.multiLayout) {
  doProduce(1, env.imageTable); // ページ数 1
 }
+
 //独自の刺し込み処理
 def myInjectionOne(cassette, record, labelList, imageTable) {
  labelList.each {

372f39418a7651c0c302e54e0552f6572968bd9d
photoretouch <photoretouch@photoretouch-no-Mac-Pro.local>
2018-08-31 18:22:48 +0900

    肩書きの自動位置調整 肩書きを下から積み上げる形に調整

--- Test/test.groovy
+++ Test/test.groovy
@@ -29,4 +29,28 @@ def myInjectionOne(cassette, record, labelList, imageTable) {
    injectionOneParts(cassette, it , record, imageTable);
  }
 
+  //表面の判定
+  def omote = getPartsByLabel('肩書き1', 1, cassette) ;
+  //表面の処理ここから
+  if(omote != null){
+    //表面のパーツ操作スクリプト
+    //関連パーツ取得
+    def title1 = getPartsByLabel('肩書き1',1,cassette);
+    def title2 = getPartsByLabel('肩書き2',1,cassette);
+    def title3 = getPartsByLabel('肩書き3',1,cassette);
+    //肩書ききが空の場合Y座標を変更
+    if(title2.param.text == ''){
+      title1.transform.translateY = 18;
+    }
+    if(title3.param.text == ''){
+      title2.transform.translateY = 21;
+      title1.transform.translateY = 18;
+    }
+    if(title2.param.text == '' && title3.param.text == ''){
+      title1.transform.translateY = 21;
+    }
+  }else{
+    //裏面のパーツ操作スクリプト
+  }
+
 }
